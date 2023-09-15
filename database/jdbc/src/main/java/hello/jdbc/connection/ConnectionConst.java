63e92ca4a062a41d7066e5480574ac2c1f9ad51f
jaykayBaek <jaykaybaek@gmail.com>
2023-09-15 22:43:06 +0900

    JDBC - insert 학습

---
+++ database/jdbc/src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public interface ConnectionConst {
+    String URL = "jdbc:h2:tcp://localhost/~/test";
+    String USERNAME = "sa";
+    String PASSWORD = "";
+}
---
+++ database/jdbc/src/main/java/hello/jdbc/domain/Member.java
@@ -0,0 +1,15 @@
+package hello.jdbc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private String memberId;
+    private int money;
+}
